c587d30b05ab63c27cf46349e2f49de8fbaf66f3
siddhartha6 <siddhartha6@missionrnd.com>
2016-04-14 14:38:08 +0530

    Submit Commit

--- src/CountPairs.cpp
+++ src/CountPairs.cpp
@@ -23,13 +23,22 @@ NOTE : Donot use any Global Variables
 #include "stdafx.h"
 
 //You can use this function ,for the actual recursion .Think of similar functions for all other problems.
-int count_pairs(char *str, int len, int start, int end){
-	return 0;
+int count_pairs(char *str, int len, int start, int end, int count){
+
+	if (end == len)  //if end value equal to length of string thrn returning count
+		return count;
+	if (str[start] == str[end])       //if alternate positions are equal incrementing count
+		count++;
+	return count_pairs(str, len, start + 1, end + 1, count);
 }
 
-int count_pairs_wrapper(char *str,int len){
-    //Wrapper function which might call a recursive function ,which might take extra parameters .
-	return 0;
+int count_pairs_wrapper(char *str, int len){
+	//Wrapper function which might call a recursive function ,which might take extra parameters .
+	if (str == NULL || len <= 2)       //zeroString case
+		return 0;
+	int result = 0;
+	result = count_pairs(str, len, 0, 2, 0); //starting comparing at alternative postions
+	return result;
 
 }
 
--- src/Steps.cpp
+++ src/Steps.cpp
@@ -1,30 +1,46 @@
 /*
 
-	You have N steps to climb .
-	You can either take one step or two steps at a time .
-	You need to find the total number of ways you can reach N steps .
+You have N steps to climb .
+You can either take one step or two steps at a time .
+You need to find the total number of ways you can reach N steps .
 
-	Example : For N=4 , You can either reach 4 steps in following ways
-	->1111
-	->112
-	->121
-	->211
-	->22
-	So total number of ways is 5
+Example : For N=4 , You can either reach 4 steps in following ways
+->1111
+->112
+->121
+->211
+->22
+So total number of ways is 5
 
-	Input : A Integer denoting N (number of steps )
-	Output : Return an Integer denoting the number of ways to reach N steps
+Input : A Integer denoting N (number of steps )
+Output : Return an Integer denoting the number of ways to reach N steps
 
-	Example Input : get_steps(5)
-	Example Output : Returns 8
+Example Input : get_steps(5)
+Example Output : Returns 8
 
-	Note : Test Cases would be small <25.
+Note : Test Cases would be small <25.
 
 */
-#include "stdafx.h"
 
-
+int test[25];
+int count_steps(int s)
+{
+	if (s <= 1)
+		return s;
+	if (test[s] != -1)
+		return test[s];         //if already test[s] value is in memory then returning it directly
+	test[s] = count_steps(s - 1) + count_steps(s - 2);     //fibonacci series
+	return test[s];
+}
 int get_steps(int s)
 {
-	return 0;
+	if (s <= 0)
+		return 0;
+	int index = 0, result = 0;
+	for (index = 0; index <= s + 1; index++)
+	{
+		test[index] = -1;
+	}
+	result = count_steps(s + 1); //no of ways of getting steps will be (s+1)th fibonacci term
+	return result;
 }
